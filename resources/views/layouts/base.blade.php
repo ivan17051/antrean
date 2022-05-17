0ed3f9e734552bb5bf0602d08157bbbceb6a0526
Ivan Indrakusuma <ivan_indrakusuma@yahoo.com>
2022-05-17 15:06:30 +0700

    Init

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Vendor styles -->
+        <link rel="stylesheet" href="{{ asset('public/vendors/material-design-iconic-font/css/material-design-iconic-font.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('public/vendors/animate.css/animate.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('public/vendors/jquery-scrollbar/jquery.scrollbar.css') }}">
+        <link rel="stylesheet" href="{{ asset('public/vendors/fullcalendar/fullcalendar.min.css') }}">
+
+        <!-- App styles -->
+        <link rel="stylesheet" href="{{ asset('public/css/app.min.css') }}">
+    </head>
+
+    <body data-ma-theme="purple">
+        <main class="main">
+            <div class="page-loader">
+                <div class="page-loader__spinner">
+                    <svg viewBox="25 25 50 50">
+                        <circle cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" />
+                    </svg>
+                </div>
+            </div>
+
+            @include('layouts.partials.navbar')
+
+            @include('layouts.partials.sidebar')
+
+            @include('layouts.partials.chat')            
+
+            <section class="content">
+                 
+                @yield('content')
+
+                @include('layouts.partials.footer')
+            </section>
+        </main>
+
+        <!-- Javascript -->
+        <!-- Vendors -->
+        <script src="{{ asset('public/vendors/jquery/jquery.min.js') }}"></script>
+        <script src="{{ asset('public/vendors/popper.js/popper.min.js') }}"></script>
+        <script src="{{ asset('public/vendors/bootstrap/js/bootstrap.min.js') }}"></script>
+        <script src="{{ asset('public/vendors/jquery-scrollbar/jquery.scrollbar.min.js') }}"></script>
+        <script src="{{ asset('public/vendors/jquery-scrollLock/jquery-scrollLock.min.js') }}"></script>
+
+        <script src="{{ asset('public/vendors/flot/jquery.flot.js') }}"></script>
+        <script src="{{ asset('public/vendors/flot/jquery.flot.resize.js') }}"></script>
+        <script src="{{ asset('public/vendors/flot.curvedlines/curvedLines.js') }}"></script>
+        <script src="{{ asset('public/vendors/jqvmap/jquery.vmap.min.js') }}"></script>
+        <script src="{{ asset('public/vendors/jqvmap/maps/jquery.vmap.world.js') }}"></script>
+        <script src="{{ asset('public/vendors/easy-pie-chart/jquery.easypiechart.min.js') }}"></script>
+        <script src="{{ asset('public/vendors/salvattore/salvattore.min.js') }}"></script>
+        <script src="{{ asset('public/vendors/sparkline/jquery.sparkline.min.js') }}"></script>
+        <script src="{{ asset('public/vendors/moment/moment.min.js') }}"></script>
+        <script src="{{ asset('public/vendors/fullcalendar/fullcalendar.min.js') }}"></script>
+
+        <!-- Charts and maps-->
+        <script src="{{ asset('public/demo/js/flot-charts/curved-line.js') }}"></script>
+        <script src="{{ asset('public/demo/js/flot-charts/dynamic.js') }}"></script>
+        <script src="{{ asset('public/demo/js/flot-charts/line.js') }}"></script>
+        <script src="{{ asset('public/demo/js/flot-charts/chart-tooltips.js') }}"></script>
+        <script src="{{ asset('public/demo/js/other-charts.js') }}"></script>
+        <script src="{{ asset('public/demo/js/jqvmap.js') }}"></script>
+
+        <!-- App functions and actions -->
+        <script src="{{ asset('public/js/app.min.js') }}"></script>
+
+        @yield('jsx')
+    </body>
+</html>
---
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -0,0 +1,59 @@
+<aside class="sidebar">
+    <div class="scrollbar-inner">
+        <div class="user">
+            <div class="user__info" data-toggle="dropdown">
+                <img class="user__img" src="public/demo/img/profile-pics/8.jpg" alt="">
+                <div>
+                    <div class="user__name">Malinda Hollaway</div>
+                    <div class="user__email">malinda-h@gmail.com</div>
+                </div>
+            </div>
+
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="">View Profile</a>
+                <a class="dropdown-item" href="">Settings</a>
+                <a class="dropdown-item" href="">Logout</a>
+            </div>
+        </div>
+
+        <ul class="navigation">
+            <li class="navigation__active"><a href="index.html"><i class="zmdi zmdi-home"></i> Home</a></li>
+
+           
+
+            <li class="navigation__sub">
+                <a href=""><i class="zmdi zmdi-collection-text"></i> Forms</a>
+
+                <ul>
+                    <li><a href="form-elements.html">Basic Form Elements</a></li>
+                    <li><a href="form-components.html">Form Components</a></li>
+                    <li><a href="form-layouts.html">Form Layouts</a></li>
+                    <li><a href="form-validation.html">Form Validation</a></li>
+                </ul>
+            </li>
+
+            
+
+            <li class="navigation__sub">
+                <a href=""><i class="zmdi zmdi-group-work"></i> Javascript Components</a>
+
+                <ul class="navigation__sub">
+                    <li><a href="carousel.html">Carousel</a></li>
+                    <li><a href="collapse.html">Collapse</a></li>
+                    <li><a href="dropdowns.html">Dropdowns</a></li>
+                    <li><a href="modals.html">Modals</a></li>
+                    <li><a href="popover.html">Popover</a></li>
+                    <li><a href="tabs.html">Tabs</a></li>
+                    <li><a href="tooltips.html">Tooltips</a></li>
+                    <li><a href="notifications-alerts.html">Notifications & Alerts</a></li>
+                    <li><a href="tree-view.html">Tree View</a></li>
+                </ul>
+            </li>
+
+           
+            <li><a href="calendar.html"><i class="zmdi zmdi-calendar"></i> Calendar</a></li>
+
+            
+        </ul>
+    </div>
+</aside>
